79762a8b957548bc10d2144f179f6ba62c754ed1
Gathindra Gamamadagedon <gathindra@gmail.com>
2021-04-17 18:39:33 -0500

    Initial commit after implementing Kotlin coroutine Channels and Flow.

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.32"
+    application
+}
+
+group = "com.gathi"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+    testImplementation(kotlin("test-junit"))
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClassName = "MainKt"
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,47 @@
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.receiveAsFlow
+import java.util.*
+import kotlin.system.exitProcess
+
+// Define the channel
+val channel = Channel<Int>()
+
+// Subscribe the channel as a flow
+val consumer = channel.receiveAsFlow()
+
+// We need to set main coroutine scope as run-blocking as we
+// need to exit the routine after consumer receive all the contents
+fun main(args: Array<String>) = runBlocking {
+
+    // Start the another coroutine scope in IO context
+    withContext(Dispatchers.IO) {
+        // Kick off consumer in another coroutine to run both
+        // consumer and producer. Otherwise consumer indefinitely
+        // wait for producer
+        launch {
+            consumeData()
+        }
+
+        doSomething()
+    }
+
+}
+
+suspend fun consumeData() = coroutineScope {
+    consumer.collect() {
+        println("Consumer received $it ${Date().toString()}")
+        if (it == 20) {
+            exitProcess(0)
+        }
+    }
+}
+
+suspend fun doSomething() = coroutineScope {
+    for (i in 1..20) {
+        channel.send(i)
+        delay(300)
+    }
+    println("Hello World!")
+}
